63103d1b46363e6bf71438851dc8048c4d60c1dc
Ashish jha <ajha5645@gmail.com>
2025-05-06 19:38:29 +0530

    Add database connection class and properties file for MySQL configuration

---
+++ src/main/java/JDBC/DBConnection.java
@@ -0,0 +1,34 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.io.InputStream;
+
+public class DBConnection {
+    private static Connection connection;
+
+    static {
+        try (InputStream input = DBConnection.class.getClassLoader().getResourceAsStream("db.properties")) {
+            Properties props = new Properties();
+            props.load(input);
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = props.getProperty("db.url");
+            String username = props.getProperty("db.username");
+            String password = props.getProperty("db.password");
+
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error while establishing database connection: " + e.getMessage());
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            throw new SQLException("Connection is not available");
+        }
+        return connection;
+    }
+}
